0b2c3686ca049c8d083968aa9f36bc32a60d766e
Elmo00 <Matusevichfizika@yandex.ru>
2023-05-26 12:37:49 +0300

    multiprocessing

--- adult_parser/parser_ajax.py
+++ adult_parser/parser_ajax.py
@@ -1,8 +1,11 @@
 import requests
 import csv
+from multiprocessing import Pool
+from time import sleep
 
 
 def get_ajax_text(url):
+    sleep(1)
     r = requests.get(url)
     return r.text
 
@@ -14,24 +17,33 @@ def write_csv(data):
         writer.writerow(data)
 
 
-def main():
-    for i in range(0, 20):
-        url = f'https://www.liveinternet.ru/rating/ru//today.tsv?page={i}'
-        response = get_ajax_text(url)
-        data = response.strip().split('\n')[1:]
-        for row in data:
-            columns = row.strip().split('\t')
-            name = columns[2]
-            url = columns[1]
-            traffic = columns[3]
-            data = {
-                'name': name,
+def get_page_data(text):
+    data = text.strip().split('\n')[1:]
+    for row in data:
+        columns = row.strip().split('\t')
+        name = columns[2]
+        url = columns[1]
+        traffic = columns[3]
+        data = {'name': name,
                 'url': url,
-                'traffic': traffic
-            }
-            write_csv(data)
+                'traffic': traffic}
+        write_csv(data)
 
-    # for i in range(0, 10):
+
+def make_all(url):
+    text = get_ajax_text(url)
+    get_page_data(text)
+
+
+def main():
+    # 4549
+    url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page={}'
+    urls = [url.format(str(i)) for i in range(0, 4549)]
+    with Pool(20) as pool:
+        pool.map(make_all, urls)
+
+    # response = get_ajax_text(url)
+     # for i in range(0, 10):
     #     write_csv(get_ajax_text(url+1))
 
 
